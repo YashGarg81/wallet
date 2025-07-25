9e456052f2d15a7612757d1ad3dd925c8afab3d5
Yash Garg <yash.garg.cse.2021@miet.ac.in>
2025-07-25 19:52:51 +0530

    Web3 based wallet project

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section className="max-w-7xl mx-auto border-t px-4">
+      <div className="flex justify-between py-8">
+        <p className="text-primary tracking-tight">
+          Designed and Developed by{" Yash "}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
